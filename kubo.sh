486c12f53bae3a0fd475a6cc255dbc6431d07e6d
Abhijeet Das <das.abhijeet@outlook.com>
2018-03-24 17:14:52 +0530

    Update kubo.sh

--- kubo.sh
+++ kubo.sh
@@ -11,3 +11,23 @@ sudo mv kubectl /usr/local/bin/kubectl
 #Installing minikube https://github.com/kubernetes/minikube/releases
 echo "Installing minikube.........................."
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.15.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+
+
+#######################################################################################
+
+minikube start
+
+kubectl run hello-minikube --image=gcr.io/google_containers/echoserver:1.4 --port=8080
+kubectl expose deployment hello-minikube --type=NodePort
+kubectl get pod
+
+sleep 30
+
+kubectl get pod
+
+curl $(minikube service hello-minikube --url)
+
+sleep 60
+
+minikube stop
